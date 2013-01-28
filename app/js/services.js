f6b91a92ced8580f938addb879494c80ec183384
hyzhak <hyzhak@ya.ru>
2013-01-28 01:29:08 +0100

    #1 update image view

--- app/js/services.js
+++ app/js/services.js
@@ -6,6 +6,7 @@ angular.module('myApp.services', ['ngResource']).
     value('version', '0.0.1').
     //better use your own client-id. Get here: http://instagram.com/developer/clients/manage/
     value('instagramClintId', '39a6f9437c464ef684d543880969764d').
+
     factory('BestOfImages', function($resource){
         return $resource('https://api.instagram.com/v1/media/popular?client_id=:clientId&redirect_uri=http://localhost:8080/&callback=JSON_CALLBACK', {}, {
             query: {method:'JSONP'}
