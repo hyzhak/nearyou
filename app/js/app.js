e89763aab8f5001207d9000a4396f66d2a1da158
Eugene Krevenets <hyzhak@ya.ru>
2013-09-23 01:21:19 +0200

    set nearyou as default

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,7 @@ var module = angular.module('myApp', ['myApp.services', 'ngRoute']).
         $routeProvider.when('/bestof', {templateUrl: 'partials/bestofinstagram.html', controller: BestOfInstagramCtrl});
         $routeProvider.when('/local', {templateUrl: 'partials/bestofinstagram.html', controller: RequestUserLocationCtrl});
         $routeProvider.when('/local/:lat/:lng', {templateUrl: 'partials/bestofinstagram.html', controller: LocalInstagramCtrl});
-        $routeProvider.otherwise({redirectTo: '/bestof'});
+        $routeProvider.otherwise({redirectTo: '/local'});
     }]);
 
 module.service('SearchState', function(){
