052ea4787f23c892603b3eef7f269faaf96e1bb6
Eugene Krevenets <hyzhak@ya.ru>
2013-11-18 00:35:26 +0100

    add deploy task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function (grunt) {
     'use strict';
 
+    var githubPagesFolder = '../gh-pages/';
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -29,6 +31,26 @@ module.exports = function (grunt) {
                 files: [
                     {expand: true, flatten: true, src: ['app/css/*.css'], dest: 'build/css/', filter: 'isFile'},
                 ]
+            },
+            gh: {
+                files: [
+                    {
+                        expand: true, flatten: false,
+                        cwd: 'build/',
+                        src: ['**'],
+                        dest: githubPagesFolder
+                    }
+                ]
+            }
+        },
+        githubPages: {
+            target: {
+                options: {
+                    // The default commit message for the gh-pages branch
+                    commitMessage: grunt.option('m') || 'update v' + grunt.file.readJSON('package.json').version
+                },
+                // The folder where your gh-pages repo is
+                src: githubPagesFolder
             }
         }
     });
@@ -36,7 +58,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-preprocess');
+    grunt.loadNpmTasks('grunt-github-pages');
 
     // Default task(s).
-    grunt.registerTask('default', ['requirejs', 'preprocess', 'copy']);
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', ['requirejs', 'preprocess', 'copy:main']);
+    grunt.registerTask('deploy', ['build', 'copy:gh', 'githubPages']);
 };
